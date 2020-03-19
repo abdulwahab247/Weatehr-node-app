8acc2f51b727e4a52d36a65fe637df9533253527
abdulwahabdev <abdulwahab@4acetech.com>
2020-03-19 16:36:28 +0500

    Initial commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYWJkdWx3YWhhYjRhY2UiLCJhIjoiY2s3bHN2dXFoMDRobjNmcDlsZzEzM3hhaCJ9.kpHuf9FkKYu38S5bLqiG1g&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
